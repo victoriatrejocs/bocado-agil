fec919529baa0ecfa99f5f3c5007f7ebf79d4f50
Victoria Trejo <victoriatrejocs@gmail.com>
2023-05-23 15:08:34 -0600

    footer y collection cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Filter from "./components/Filter"
+
 const App = () => {
   return (
+    <>
     <Navbar/>
+    <Filter/>
+    <Footer/>
+    </>
   )
 }
 export default App
